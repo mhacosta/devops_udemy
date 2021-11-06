62c3b861f599f1e6d0a5e3e21e66d8a5713ac3ba
udemy prueba <udemyprueba@domai.com>
2021-11-06 10:28:04 -0300

    add test modification

--- billing/src/test/java/com/paymentchain/billing/BasicApplicationTests.java
+++ billing/src/test/java/com/paymentchain/billing/BasicApplicationTests.java
@@ -1,4 +1,4 @@
-package com.paymentchain.billing;
+﻿package com.paymentchain.billing;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -11,6 +11,8 @@ public class BasicApplicationTests {
 
 	@Test
 	public void contextLoads() {
+		    	String message= “default message cambio 11”;
+			Assert.assertNotNull(message);
 	}
 
 }
